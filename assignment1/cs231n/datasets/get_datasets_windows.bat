9593ecdd318a8394dd8d7c209a47b22a05ecc704
cswu <xray0h@gmail.com>
2018-03-13 15:55:40 +0800

    add windows download support

---
+++ assignment1/cs231n/datasets/get_datasets_windows.bat
@@ -0,0 +1,5 @@
+bin\wget.exe http://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz
+bin\7z e .\cifar-10-python.tar.gz
+bin\7z x .\cifar-10-python.tar
+del cifar-10-python.tar.gz 
+del cifar-10-python.tar
